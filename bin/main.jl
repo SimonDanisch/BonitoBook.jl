747f5c678e3cf8206c997726fa26ddabf59c9e81
SimonDanisch <sdanisch@protonmail.com>
2025-08-03 18:34:35 +0200

    fix app

--- bin/main.jl
+++ bin/main.jl
@@ -9,12 +9,12 @@ end
 port = get(ENV, "PORT", "8080") # it's guaranteed this exists on JuliaHub
 @info "Constructing Bonito server on 0.0.0.0:$port $(isempty(proxy) ? "" : "with proxy $proxy")"
 server = Bonito.Server("0.0.0.0", parse(Int, port); proxy_url=proxy, verbose=-1)
-path = joinpath(@__DIR__, "..", "docs", "examples", "intro.md")
+path = normpath(joinpath(dirname(pathof(BonitoBook)), "..", "docs", "examples", "intro.md"))
 app = Bonito.App() do
     return BonitoBook.Book(path)
 end;
 route!(server, "/" => app)
-
+@info server
 # Wait for the server to exit, because if running in an app, the app will
 # exit when the script is done.  This makes sure that the app is only closed
 # if (a) the server closes, or (b) the app itself times out and is killed externally.
