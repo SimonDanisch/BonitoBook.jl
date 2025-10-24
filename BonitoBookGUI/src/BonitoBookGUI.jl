762255fe5b0fce20875aaf1558f6f5780ebed52c
Janis Erdmanis <janiserdmanis@protonmail.ch>
2025-10-25 01:40:01 +0300

    instantiating examples into user data directory

--- BonitoBookGUI/src/BonitoBookGUI.jl
+++ BonitoBookGUI/src/BonitoBookGUI.jl
@@ -1,7 +1,9 @@
 module BonitoBookGUI
 
-# Currently AppBundler precompiles only dependencies that are launched from the main app module
-# This fix is temporary until we figure out details within AppBundler on how to do this properly
+# Currently AppBundler only precompiles the main app module and its direct dependencies
+# (including transitive dependencies of those direct dependencies).
+# Weak dependencies are not precompiled since they're not in the dependency tree.
+# This is a temporary workaround until AppBundler properly handles precompilation of weak dependencies.
 import Sunny, GeometryBasics, WGLMakie, DataFrames
 
 # BonitoBook Welcome Page Server
@@ -22,8 +24,22 @@ in the docs/examples directory. The server runs until the window is closed.
 - `args`: Command line arguments (currently unused)
 """
 function (@main)(args)
+
     # Get the examples folder path
     examples_folder = normpath(joinpath(dirname(pathof(BonitoBook)), "..", "docs", "examples"))
+
+    # Copy examples to a mutable user data directory
+    if haskey(ENV, "USER_DATA")
+        user_examples = joinpath(ENV["USER_DATA"], "examples")
+        @info "Using examples stored in user data directory $user_examples"
+        if !isdir(user_examples)
+            @info "Instantiating examples into user data directory"
+            mkpath(ENV["USER_DATA"])
+            cp(examples_folder, user_examples)
+            chmod(user_examples, 0o755; recursive=true)
+        end
+        examples_folder = user_examples
+    end
 
     if !isdir(examples_folder)
         error("Examples folder not found: $examples_folder")
